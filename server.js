cf8622db7e4e774beef14e3ee51e2c3a634f3f03
Clyde-Matthew <clyde.matthew1@gmail.com>
2022-04-27 20:20:37 +0200

    database connecting

--- server.js
+++ server.js
@@ -10,15 +10,13 @@ const profile = require("./controllers/profile");
 const image = require("./controllers/image");
 
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 
 const db = require('knex')({
   client: 'pg',
   connection: {
-    connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+    host: process.env.DATABASE_URL,
+  ssl: true 
   }
 });
 
