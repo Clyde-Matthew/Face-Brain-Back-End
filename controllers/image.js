03d15523fb9b42ed85741c5d6c2897f01ac056b6
Clyde-Matthew <clyde.matthew1@gmail.com>
2022-04-27 21:10:02 +0200

    hiding API Key

--- controllers/image.js
+++ controllers/image.js
@@ -3,7 +3,7 @@ const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
 const stub = ClarifaiStub.grpc();
 
 const metadata = new grpc.Metadata();
-metadata.set("authorization", `Key ${process.env.API_KEY}`);
+metadata.set("authorization", `Key ${process.env.API_CLARIFAI}`);
 
 // const Clarifai = require('clarifai');
 // console.log(Clarifai);
